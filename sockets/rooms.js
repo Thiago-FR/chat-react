c74a8417afccd0f0597a416747070415518ae134
Thiago <thiago_ssfr@hotmail.com>
2022-06-27 23:12:54 -0300

    create admin zone

--- sockets/rooms.js
+++ sockets/rooms.js
@@ -1,9 +1,19 @@
+let rooms = [];
+let socketAdmin = null;
+
 module.exports = (io) => io.on('connection', (socket) => {
   socket.on('joinRoom', ({ username, room, color }) => {
+    newRoom = rooms.find((sala) => sala.room && sala.room === room)
+    if (newRoom) {
+      newRoom.members += 1;
+      newRoom.users = [...newRoom.users, { username }];
+    } else rooms.push({ users: [{ username }], room, members: 1 });
+
+    if (socketAdmin) socketAdmin.emit('add', rooms);
     socket.join(room);
 
     socket.emit('serverMessage', {
-      message: `Bem Vindo ${username} a sala Secreta -> ${room}`,
+      message: `Bem Vindo ${username} a sala Secreta ↪ ${room} ↩`,
       className: 'server-emit-me'
     });
 
@@ -38,11 +48,30 @@ module.exports = (io) => io.on('connection', (socket) => {
       });
     });
 
+    socket.on('exit', () => {
+      socket.disconnect(room);
+    });
+
     socket.on('disconnect', () => {
+      newRoom = rooms.find((sala) => sala.room && sala.room === room)
+      if (newRoom.members > 1) {
+        const { users } = newRoom;
+        newRoom.members -= 1;
+        newRoom.users = users.filter((user) => user.username !== username);
+      } else rooms = rooms.filter((sala) => sala.room !== room);
+      
       socket.broadcast.to(room).emit('serverMessage', {
         message: `${username} acabou de sair da sala`,
         className: 'server-emit-room room-out'
       });
+
+      if (socketAdmin)  socketAdmin.emit('add', rooms);
     });
-  });  
+  });
+
+  socket.on('joinAdmin', () => {
+    socket.join('adminMaster')
+    socketAdmin = socket;
+    socket.emit('add', rooms);
+  });
 });
